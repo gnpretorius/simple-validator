53ac44e5ea5c17bcdc36f6b8bf9c20dfb6914339
Gordon Pretorius <g.pretorius@cergis.com>
2015-05-07 16:28:49 +0100

    Added Datetime checks

--- src/SimpleValidator/Extensions/ExtensionMethods.cs
+++ src/SimpleValidator/Extensions/ExtensionMethods.cs
@@ -59,6 +59,22 @@ namespace SimpleValidator.Extensions
         }
 
         /// <summary>
+        /// Checks to see if the current object is null, or if it's a string, if it's empty
+        /// </summary>
+        /// <param name="value">The object to check</param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this object value) // this is a shortcut method to the above
+        {
+            if (value == null)
+                return true;
+
+            if (value is string)
+                return ((string)value).IsEmpty();
+
+            return false;
+        }
+
+        /// <summary>
         /// Check if the current value is a valid email address. It uses the following regular expression
         /// ^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$
         /// </summary>
