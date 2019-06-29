f6534cc17e652081f0200ab37078034ba591dbc1
zhouguoqing <zhouguoqing.eric@gmail.com>
2019-06-29 17:50:30 +0800

    ValidationException support  Serializable

--- src/SimpleValidator/Exceptions/ValidationException.cs
+++ src/SimpleValidator/Exceptions/ValidationException.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 using System.Text;
 
 namespace SimpleValidator.Exceptions
 {
+    [Serializable]
     public class ValidationException : Exception
     {
         public Validator Validator;
@@ -16,6 +19,28 @@ namespace SimpleValidator.Exceptions
         public ValidationException(Validator validator) : base()
         {
             Validator = validator;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="info">Serialization Info</param>
+        /// <param name="context">Streaming Context</param>
+        protected ValidationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Get object data
+        /// </summary>
+        /// <param name="info">Serialization Info</param>
+        /// <param name="context">context</param>
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
         }
     }
 }
